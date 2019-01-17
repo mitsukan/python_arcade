dd6c0b7005040d6c1dd701e123fbf770dc813bb7
Jihin Ip <jim_ip20@hotmail.com>
2019-01-17 13:17:11 +0000

    Defining a tree method to draw a tree

--- basic_background.py
+++ basic_background.py
@@ -8,6 +8,20 @@ def draw_background():
     arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_HEIGHT*(1/3), arcade.color.SKY_BLUE)
     arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT/3, 0, arcade.color.DARK_SPRING_GREEN)
 
+def draw_tree(x, y):
+    # draw the leafy stuff
+    arcade.draw_triangle_filled(
+        x + 40, y,
+        x, y - 100,
+        x + 80, y - 100,
+        arcade.color.DARK_GREEN
+    )
+    # draw the trunk
+    arcade.draw_rectangle_filled(
+        x + 40, y -125,
+        20, 50,
+        arcade.color.DARK_BROWN
+    )
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Basic background")
@@ -15,6 +29,7 @@ def main():
     arcade.start_render()
 
     draw_background()
+    draw_tree(400, 280)
 
     arcade.finish_render()
 
