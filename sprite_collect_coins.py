19295a6977bdbfd56cf5998c3c8dbb8890dbffb3
Jihin Ip <jim_ip20@hotmail.com>
2019-01-19 18:04:16 +0000

    Adding the arcade.Window class to manage the application

--- sprite_collect_coins.py
+++ sprite_collect_coins.py
@@ -1,3 +1,41 @@
-SPITE_COIN_SCALING = 0.2
+import arcade
 
-coin = arcade.Sprite('')
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SPRITE_COIN_SCALING = 0.2
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        # Set up your game here
+
+        pass
+
+
+    def on_draw(self):
+        """ Render the screen. """
+        arcade.start_render()
+        # Your drawing code goes here
+
+    def update(self, delta_time):
+        """ All the logic to move, and the game logic goes here. """
+        pass
+
+
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
+
+
