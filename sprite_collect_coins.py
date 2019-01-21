600892032816c8fae48462d6cef75a244e211acf
Jihin Ip <jim_ip20@hotmail.com>
2019-01-21 15:19:20 +0000

    Adding a sprite to the window via arcade.Sprite and sprite lists

--- sprite_collect_coins.py
+++ sprite_collect_coins.py
@@ -1,6 +1,6 @@
 import arcade
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SPRITE_COIN_SCALING = 0.2
 
@@ -11,18 +11,25 @@ class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
+        # Instantiate the class with a player list. List is used to keep postition of the sprite(s)
+        self.player_list = None
+        self.player_sprite = None
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-        # Set up your game here
-
-        pass
-
+        # start a new sprite list
+        self.player_list = arcade.SpriteList()
+        # set up the player sprite
+        self.player_sprite = arcade.Sprite("images/rabbitstand120.gif", SPRITE_COIN_SCALING)
+        self.player_sprite.center_x = 300
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
-        # Your drawing code goes here
+        self.player_list.draw()
+
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
