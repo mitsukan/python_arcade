4d9059ac1f6ddd6007ec8080d0bb58cdeab5a949
Jihin Ip <jim_ip20@hotmail.com>
2019-01-22 20:52:47 +0000

    Adding comments

--- sprite_collect_coins.py
+++ sprite_collect_coins.py
@@ -9,6 +9,7 @@ class MyGame(arcade.Window):
     """ Main application class. """
 
     def __init__(self, width, height):
+        # Use super to inherit arcade.Window's properties
         super().__init__(width, height)
 
         # Instantiate the class with a player list. List is used to keep postition of the sprite(s)
@@ -23,14 +24,18 @@ class MyGame(arcade.Window):
         self.player_sprite = arcade.Sprite("images/rabbitstand120.gif", SPRITE_COIN_SCALING)
         self.player_sprite.center_x = 300
         self.player_sprite.center_y = 300
+        # Adding the player sprite to the list
         self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
+        # Draw the list
         self.player_list.draw()
 
     def on_mouse_motion(self, x, y, dx, dy):
+        # This is an override of the method to activate the functionality.
+        # On mouse motion, update the player sprite with the following:
         self.player_sprite.center_x = x
         self.player_sprite.center_y = y
 
